552a627f40c86d820c7a0e05b886d6af4d3e0296
Deceptrax123 <sriniishsri@gmail.com>
2023-08-15 18:27:52 +0530

    model fixes

--- Gan/art-generation/discriminator.py
+++ Gan/art-generation/discriminator.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn 
 
 from torch.nn import Module 
-from torch.nn import Conv2d,BatchNorm2d,Flatten,Sigmoid,LeakyReLU,ConvTranspose2d,Linear
+from torch.nn import Conv2d,BatchNorm2d,Flatten,Sigmoid,LeakyReLU,ConvTranspose2d,Linear,Dropout2d
 from prettytable import PrettyTable
 
 from torchsummary import summary
@@ -12,44 +12,40 @@ class Discriminator(Module):
         super(Discriminator,self).__init__()
 
         self.conv1=Conv2d(in_channels=3,out_channels=8,stride=2,kernel_size=(3,3),padding=1)
+        self.dp1=Dropout2d(p=0.2)
         self.lr1=LeakyReLU(negative_slope=0.2)
 
         self.conv2=Conv2d(in_channels=8,out_channels=16,stride=2,kernel_size=(3,3),padding=1)
+        self.dp2=Dropout2d(p=0.2)
         self.bn2=BatchNorm2d(num_features=16)
         self.lr2=LeakyReLU(negative_slope=0.2)
 
         self.conv3=Conv2d(in_channels=16,out_channels=32,kernel_size=(3,3),stride=2,padding=1)
+        self.dp3=Dropout2d(p=0.2)
         self.bn3=BatchNorm2d(32)
         self.lr3=LeakyReLU(negative_slope=0.2)
 
-        self.conv4=Conv2d(in_channels=32,out_channels=64,kernel_size=(3,3),stride=2,padding=1)
-        self.bn4=BatchNorm2d(64)
-        self.lr4=LeakyReLU(negative_slope=0.2)
-
         self.flatten=Flatten(start_dim=1)
-        self.linear=Linear(4096,1)
+        self.linear=Linear(8192,1)
 
-        self.sigmoid=Sigmoid()
     
     def forward(self,x):
         x=self.conv1(x)
+        x=self.dp1(x)
         x=self.lr1(x)
 
         x=self.conv2(x)
+        x=self.dp2(x)
         x=self.bn2(x)
         x=self.lr2(x)
 
         x=self.conv3(x)
+        x=self.dp3(x)
         x=self.bn3(x)
         x=self.lr3(x)
 
-        x=self.conv4(x)
-        x=self.bn4(x)
-        x=self.lr4(x)
-
         x=self.flatten(x)
         x=self.linear(x)
-        x=self.sigmoid(x)
 
         return x
 
@@ -68,6 +64,6 @@ class Discriminator(Module):
 
 
 # model = Discriminator()
-# # # count_parameters(model)
+# # # # count_parameters(model)
 
 # summary(model,input_size=(3,128,128),batch_size=8,device='cpu')
--- Gan/art-generation/training_loops.py
+++ Gan/art-generation/training_loops.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from torch import nn
 from time import time
 import multiprocessing as mp
-
+import torch.multiprocessing
 
 def train_step():
     gen_loss=0
@@ -56,76 +56,41 @@ def train_step():
 
     return gen_loss,dis_loss
 
-def test_step():
-    gen_loss=0
-    dis_loss=0
-
-    for step,data in enumerate(test_loader):
-        real_samples=data.to(device=device)
-        real_labels=torch.ones((real_samples.size(0),1)).to(device=device)
-
-        #generate samples in latent space
-        latent_space_samples=torch.randn((real_samples.size(0),100)).to(device=device)
-        generated_samples=generator(latent_space_samples)
-
-        generated_labels=torch.zeros((real_samples.size(0),1)).to(device=device)
-
-        #consider all samples for discriminator loss
-        samples=torch.cat((real_samples,generated_samples))
-        labels=torch.cat((real_labels,generated_labels))
-
-        #get discriminator loss
-        discriminator_output=discriminator(samples)
-        loss_discriminator=loss_function(discriminator_output,labels)
-
-        #get generator loss
-        latent_space_samples=torch.randn((real_samples.size(0),100)).to(device=device)
-        generated_samples=generator(latent_space_samples)
-
-        discriminator_generated=discriminator(generated_samples)
-        loss_generator=loss_function(discriminator_generated,real_labels)
-
-        gen_loss+=loss_generator.item()
-        dis_loss+=loss_discriminator.item()
-
-        if (step==test_steps-1):
-            gen_loss=gen_loss/(step+1)
-            dis_loss=dis_loss/(step+1)
-    return gen_loss,dis_loss
-
 def training_loop():
 
+    glosses=[]
+    dlosses=[]
     for epoch in range(num_epochs):
         generator.train(True)
         discriminator.train(True)
 
         train_losses=train_step()
 
-        generator.eval()
-        discriminator.eval()
-
-        test_losses=test_step()
+        generator.train(False)
+        discriminator.train(False) 
         
         print('Epoch {epoch}'.format(epoch=epoch+1))
-        print("Generator Loss: Train - {gen} Test- {dis}".format(gen=train_losses[0],dis=test_losses[0]))
-        print("Discriminator Loss: Train - {gen} Test-{dis}".format(gen=train_losses[1],dis=test_losses[1]))
+        print("Generator Loss: {gloss}".format(gloss=train_losses[0]))
+        print("Discriminator Loss: {dloss}".format(dloss=train_losses[1]))
+
+        dlosses.append(train_losses[1])
+        glosses.append(train_losses[0])
+
 
 if __name__=='__main__':
+    torch.multiprocessing.set_sharing_strategy('file_system')
     #initial setup
     ids = list(range(0, 2782))
-    train,test=train_test_split(ids,test_size=0.25)
 
     params={
-        'batch_size':8,
+        'batch_size':128,
         'shuffle':True,
         'num_workers':4
     }
 
-    train_dataset=Dataset(train)
-    test_dataset=Dataset(test)
+    dataset=Dataset(ids)
 
-    train_loader=DataLoader(train_dataset,**params)
-    test_loader=DataLoader(test_dataset,**params)
+    train_loader=DataLoader(dataset,**params)
 
     #device usage 
     if torch.backends.mps.is_available():
@@ -138,19 +103,22 @@ if __name__=='__main__':
 
     #hyperparameters
     lr=0.0002
-    num_epochs=100
-    loss_function=nn.BCELoss()
+    num_epochs=50
+    loss_function=nn.BCEWithLogitsLoss()
 
     #set optimizer
     generator_optimizer=torch.optim.Adam(generator.parameters(),lr=lr,betas=(0.5,0.999))
     discriminator_optimizer=torch.optim.Adam(discriminator.parameters(),lr=lr,betas=(0.5,0.999))
 
 
-    train_steps=(len(train)+params['batch_size']-1)//params['batch_size']
-    test_steps=(len(test)+params['batch_size']-1)//params['batch_size']
+    train_steps=(len(ids)+params['batch_size']-1)//params['batch_size']
 
-    training_loop()
+    history=training_loop()
 
+    torch.save(generator.state_dict(),'generator.pth')
+    torch.save(discriminator.state_dict(),'discriminator.pth')
+
+    #get ideal count of num_workers
     # for num_workers in range(2, mp.cpu_count()+2, 2):  
     #     tloader = DataLoader(train_dataset,shuffle=True,num_workers=num_workers,batch_size=32,pin_memory=True)
     #     start = time()
